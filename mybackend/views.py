5e755b6d1a2881c1189af92439cd073c4b94c5ce
ryo121w <ryoutarou9121@icloud.com>
2023-09-18 11:42:52 +0900

    commmit

--- mybackend/views.py
+++ mybackend/views.py
@@ -26,7 +26,7 @@ from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser
 from rest_framework.parsers import FormParser
 from rest_framework import status
-from rest_framework.viewsets import ViewSet
+from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view
 from .models import UploadedFile
@@ -53,8 +53,7 @@ def my_view(request):
     return response
 
 
-@api_view(['GET', 'POST'])
-class SpectrumViewSet(ViewSet):
+class SpectrumViewSet(viewsets.ModelViewSet):
     queryset = Spectrum.objects.all().order_by('wavelength')
     serializer_class = SpectrumSerializer
 
